1ae933a28219f59f1c100a2f24152c2d3609a0bf
lylehopkins <57555267+lylehopkins@users.noreply.github.com>
2021-03-09 09:56:26 -0800

    Update script.js

--- script.js
+++ script.js
@@ -5,8 +5,8 @@ return items[Math.floor(Math.random()*items.length)];
      
 }
 
-var items = ['who is listening?', 'who is the listener?', 'what is sounding?', 'what is the quality of a sound you hear (real or imagined)?', 'what is the character of a sound you hear (real or imagined)?', 'where is the listener?', 'where do sounds arise?', 'when do sounds arise?', 'why judge sounds?', 'how do sounds arise?'];
+let items = ['who is listening?', 'who is the listener?', 'what is sounding?', 'what is the quality of a sound you hear (real or imagined)?', 'what is the character of a sound you hear (real or imagined)?', 'where is the listener?', 'where do sounds arise?', 'when do sounds arise?', 'why judge sounds?', 'how do sounds arise?'];
 
-var generatedText = random_item(items);
+let generatedText = random_item(items);
     myText = document.createTextNode(generatedText);
 document.body.appendChild(myText);
